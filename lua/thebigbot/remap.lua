24fab3c81b7f57279bbc3f22d4a281ab0b206392
vclemenzi <vclemenzi@users.noreply.github.com>
2023-08-15 08:44:43 +0200

    added a keymap

--- lua/thebigbot/remap.lua
+++ lua/thebigbot/remap.lua
@@ -9,5 +9,11 @@ vim.api.nvim_set_keymap('n', '<leader>o', [[<cmd>lua require('telescope.builtin'
 vim.api.nvim_set_keymap('n', '<leader>b', [[<cmd>lua require("buffer_manager.ui").toggle_quick_menu()<CR>]],
   { noremap = true, silent = true })
 
+-- Code
 vim.api.nvim_set_keymap('n', '<leader>xx', [[<cmd>TroubleToggle<CR>]],
+  { noremap = true, silent = true })
+
+
+-- Tabs
+vim.api.nvim_set_keymap('n', '<leader>tn', [[<cmd>tabnew<CR>]],
   { noremap = true, silent = true })
